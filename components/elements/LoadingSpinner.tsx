f4df1f361ff437977fc3e4075f104a647b03ef6a
Anis Dzed <anisdzed@gmail.com>
2025-10-31 00:25:49 +0100

    add loading spinner component

--- components/elements/LoadingSpinner.tsx
+++ components/elements/LoadingSpinner.tsx
@@ -1,5 +1,15 @@
 const LoadingSpinner = () => {
-  return <div>LoadingSpinner</div>;
+  return (
+    <div className="flex justify-center items-center py-10 md:py-20">
+      <div
+        className="w-16 h-16 border-4 border-t-4 border-t-white border-blue-400 rounded-full animate-spin"
+        role="status"
+        aria-label="Loading weather data"
+      >
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
 };
 
 export default LoadingSpinner;
